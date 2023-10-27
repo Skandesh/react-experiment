89789ca391cd407b57bda863c763fdbcc6a226b8
Skandesh <42321593+Skandesh@users.noreply.github.com>
2023-10-28 00:15:34 +0530

    laying foundation

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-const h11 = React.createElement("h1", { id: "title" }, "React has loaded now");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const rooot = ReactDOM.createRoot(document.getElementById("root"));
-debugger;
-rooot.render(h11);
+const heading = <h1>Hello from JSX</h1>;
+
+const elem = <span>Hey span</span>;
+const title = <h1 className="head">{elem}</h1>;
+
+const HeadingComponent = () => (
+  <div>
+    <h1>Namaste Functional component </h1>
+    {title}
+    {heading}
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HeadingComponent />
+  //{HeadingComponent()}
+  //<HeadingComponent></HeadingComponent>
+);
